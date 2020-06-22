23b738ed74673c328d40032cf6e9510c2e8ab432
Mateusz Kopij <sagasu@gmail.com>
2020-06-22 12:26:42 +0100

    Updates

---
+++ precache-manifest.c516873146bc26b2219c12172374c265.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a75a62109967d8aa629d3eb976cca9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a818edf0f027765f8482",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "19e47533f4eea532168c",
+    "url": "/static/js/2.b7751cbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b7751cbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a818edf0f027765f8482",
+    "url": "/static/js/main.30ee462d.chunk.js"
+  },
+  {
+    "revision": "2eb98f98268de2bf97cd",
+    "url": "/static/js/runtime-main.d7e8f8c4.js"
+  }
+]);
