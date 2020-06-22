85eb30321cf9defa460749d51c1c77e8495a8068
Mateusz Kopij <sagasu@gmail.com>
2020-06-22 12:30:14 +0100

    adding gh pages

--- reed/src/App.js
+++ reed/src/App.js
@@ -4,19 +4,21 @@ import axios from 'axios';
 
 export default function App() {
     const [results, setResults] = useState([]);
-
-    useEffect(() => {
-        getResults();
-    }, []);
+    const [query, setQuery] = useState('reacthooks'); 
 
     const getResults = async () => {
-        const response = await axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks');
+        const response = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`);
         console.log(response.data);
         setResults(response.data.hits);
     };
 
+    useEffect(() => {
+        getResults();
+    }, [query]);
+
     return (
         <>
+            <input type="test" onChange={event => setQuery(event.target.value)} value={query}></input>
             <ul>
                 {results.map(result => (
                     <li key={result.objectID}>
