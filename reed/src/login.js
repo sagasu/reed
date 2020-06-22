3a1d6279c337835e6f7130bf3a9afd42f1683d88
Mateusz Kopij <sagasu@gmail.com>
2020-06-22 07:51:21 +0100

    login functionality implemented

--- reed/src/login.js
+++ reed/src/login.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [user, setUser] = useState(null);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const userData = {
+            username,
+            password
+        };
+        setUser(userData);
+        setUsername(username);
+        setPassword(password);
+    };
     return (
         <div
             style={{
@@ -12,12 +26,14 @@ export default () => {
                 display: 'grid',
                 alignItems: 'center',
                 justifyItems: 'center'
-            }}>
-                <input type="text" placeholder="Username"/>
-                <input type="text" placeholder="Password"/>
+            }}
+                onSubmit={handleSubmit}
+            >
+                <input type="text" value={username} placeholder="Username" onChange={event => setUsername(event.target.value)}/>
+                <input type="password" value={password} placeholder="Password" onChange={event => setPassword(event.target.value)}/>
                 <button type="submit">submit</button>
             </form>
-
+            {user && JSON.stringify(user, null, 2)}
         </div>
         
     );
