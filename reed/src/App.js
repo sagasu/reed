3211f311dfaa70dc5abc34c89acf53c1ed10e782
Mateusz Kopij <sagasu@gmail.com>
2020-06-22 12:01:51 +0100

    async call to HN api

--- reed/src/App.js
+++ reed/src/App.js
@@ -6,12 +6,14 @@ export default function App() {
     const [results, setResults] = useState([]);
 
     useEffect(() => {
-        axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
-            .then(response => {
-                console.log(response.data);
-                setResults(response.data.hits);
-            });
+        getResults();
     }, []);
+
+    const getResults = async () => {
+        const response = await axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks');
+        console.log(response.data);
+        setResults(response.data.hits);
+    };
 
     return (
         <>
