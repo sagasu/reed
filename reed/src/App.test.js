b44f4bdfda9c827b2faa644e3f7d8953edee7c0b
Mateusz Kopij <sagasu@gmail.com>
2020-06-23 14:15:19 +0100

    adding enzyme and eslint rules

--- reed/src/App.test.js
+++ reed/src/App.test.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-import Enzyme, {shallow} from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
+let wrapper;
 
 describe("Elements testing", () => {
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
   test('renders Search button', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find('form').text()).toContain('Search');
   });
 
   test('render input with text', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find('#search-btn').text()).toBe('Search');
   });
 });
