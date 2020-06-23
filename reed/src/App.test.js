bbc8654651761a7f3c52b68208a800277d4a608c
Mateusz Kopij <sagasu@gmail.com>
2020-06-23 10:57:51 +0100

    resolving problems to be able to run test in Jest

--- reed/src/App.test.js
+++ reed/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Search button', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const searchButton = getByText(/Search/i);
+  expect(searchButton).toBeInTheDocument();
 });
