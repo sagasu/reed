ca37c6a2259a16f100526b7b94f2ca4f924ad77c
Mateusz Kopij <sagasu@gmail.com>
2020-06-29 09:10:27 +0100

    adding error handling

--- reed/src/App.js
+++ reed/src/App.js
@@ -6,13 +6,18 @@ export default function App() {
     const [results, setResults] = useState([]);
     const [query, setQuery] = useState('reacthooks');
     const [loading, setLoading] = useState('false');
+    const [error, setError] = useState(null);
     const searchInputRef = useRef()
 
     const getResults = async () => {
         setLoading(true);
-        const response = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`);
-        console.log(response.data);
-        setResults(response.data.hits);
+        try{
+            const response = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`);
+            setResults(response.data.hits);
+        }catch(err){
+            setError(err);
+        }
+        
         setLoading(false);
     };
 
@@ -45,6 +50,7 @@ export default function App() {
                 ))}
             </ul>
             }
+            {error && <div>{error.message}</div>}
         </>
     );
 }
